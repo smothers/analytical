974583e1d4b73a617a01ecba5afecc59c5f04928
alexanderfabry <alexander@artsymail.com>
2011-12-19 16:47:29 -0500

    Added support for custom_variables in Google Analytics

--- lib/analytical/modules/google.rb
+++ lib/analytical/modules/google.rb
@@ -3,6 +3,11 @@ module Analytical
     class Google
       include Analytical::Modules::Base
 
+       # slot: 1-5 allowed
+      # key: String
+      # value: String
+      # scope: 1 (visitor-level), 2 (session-level), or 3 (page-level)
+      # custom_variables: Array of Objects :custom_variables => [{:slot => 1, :key => "key1", :value => "value1", :scope => 3}, {:slot => 2, :key => "key2", :value => "value2", :scope => 1}]
       def initialize(options={})
         super
         @tracking_command_location = :head_append
@@ -17,7 +22,16 @@ module Analytical
             _gaq.push(['_setAccount', '#{options[:key]}']);
             _gaq.push(['_setDomainName', '#{options[:domain]}']);
             #{"_gaq.push(['_setAllowLinker', true]);" if options[:allow_linker]}
-            _gaq.push(['_trackPageview']);
+            #{"_gaq.push(['_trackPageLoadTime']);" if options[:track_page_load_time]}
+            console.log("------------- custom_variables");
+            console.log(custom_variables);
+            if(typeof custom_variables !== 'undefined'){
+              for( custom_variable in custom_variables){
+                console.log(custom_variable);
+                _gaq.push(['_setCustomVar', custom_variable.slot, custom_variable.key, custom_variable.value, custom_variable.scope ]);
+              }
+            }
+             _gaq.push(['_trackPageview']);
             (function() {
               var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
               ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
@@ -29,17 +43,25 @@ module Analytical
         end
       end
 
+      # slot: 1-5 allowed
+      # key: String
+      # value: String
+      # scope: 1 (visitor-level), 2 (session-level), or 3 (page-level)
+      def custom_variable(slot, key, value, scope)
+        "_gaq.push(['_setCustomVar', #{slot}, '#{key}', '#{value}', #{scope} ]);"
+      end
+
       def track(*args)
         "_gaq.push(['_trackPageview'#{args.empty? ? ']' : ', "' + args.first + '"]'});"
       end
-      
+
       def event(name, *args)
         data = args.first || {}
         data = data[:value] if data.is_a?(Hash)
         data_string = !data.nil? ? ", #{data}" : ""
-        "_gaq.push(['_trackEvent', \"Event\", \"#{name}\"" + data_string + "]);"
+        "_gaq.push(['_trackEvent', \"#{name}\" #{data_string} , \"test\", 1 ]);"
       end
-      
+
     end
   end
 end
